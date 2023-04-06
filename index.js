f0d68022fd83820897857ea243bb8749917f728a
atanu5arkar <atanu143sarkar@gmail.com>
2023-04-06 21:04:44 +0530

    version3
    finishes the project

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
-//submit btns functionality
+/* submit btns */
 
 $(".submit").click(function() {
+    
     if($(this).hasClass("sub1")) {
         $(".details1").toggleClass("hide");
         $(".details2").toggleClass("show");
@@ -20,9 +21,10 @@ $(".submit").click(function() {
     }
 });
 
-//back btns functionality
+/* back btns */
 
 $(".back").click(function() {
+    
     if($(this).hasClass("bk2")) {
         $(".details1").toggleClass("hide");
         $(".details2").toggleClass("show");
@@ -37,34 +39,72 @@ $(".back").click(function() {
     }
 });
 
-//step hover states
+/* to be able to click on step names, and making sure that a user cant't
+skip steps */
 
-$(".step2, .step3").hover(
+$(".name").click(function() {
 
-    function() {
-        
-        if($(this).hasClass("step2")) {
-            if(!$(".num2").hasClass("activeNum")) {
-                $(".num2, .name2").toggleClass("activeName");
-            }
-        }
-        else {
-            if(!$(".num3").hasClass("activeNum")) {
-                $(".num3, .name3").toggleClass("activeName");
-            }
-        }
-    },
-    function() {
+    if($(this).hasClass("name1")) {
 
-        if($(this).hasClass("step2")) {
-            if(!$(".num2").hasClass("activeNum")) {
-                $(".num2, .name2").toggleClass("activeName");
-            }
-        }
-        else {
-            if(!$(".num3").hasClass("activeNum")) {
-                $(".num3, .name3").toggleClass("activeName");
-            }
+        if($(".details2").hasClass("show")) {
+            $(".details1").toggleClass("hide");
+            $(".details2").toggleClass("show");
+            $(".num2").toggleClass("activeNum");
+            $(".name2").toggleClass("activeName");
         }
     }
-);
+    else if($(this).hasClass("name2")) {
+        
+        
+        if (!$(".details1").hasClass("hide")) {
+            $(".details1").toggleClass("hide");
+            $(".details2").toggleClass("show");
+            $(".num2").toggleClass("activeNum");
+            $(".name2").toggleClass("activeName");
+        }
+        else if ($(".details3").hasClass("show")) {            
+            $(".details3").toggleClass("show");
+            $(".num3").toggleClass("activeNum");
+            $(".name3").toggleClass("activeName");
+            $(".details2").toggleClass("show");           
+        }
+    }
+    else {        
+        if ($(".details2").hasClass("show")) {
+            $(".details2").toggleClass("show");
+            $(".details3").toggleClass("show");
+            $(".num3").toggleClass("activeNum");
+            $(".name3").toggleClass("activeName");
+        }
+    }    
+});
+
+/* hover states for the last two step names */
+
+$(".name").mouseenter(function() {
+
+    if($(this).hasClass("name2")) {
+        if(!$(".name2").hasClass("activeName")) {
+            $(".num2, .name2").toggleClass("hoverColor");
+        }
+    }
+    else if($(this).hasClass("name3")) {
+        if(!$(".name3").hasClass("activeName")) {
+            $(".num3, .name3").toggleClass("hoverColor");
+        }
+    }
+});
+
+$(".name").mouseleave(function() {
+
+    if($(this).hasClass("name2")) {
+        if(!$(".num2").hasClass("activeNum")) {
+            $(".num2, .name2").toggleClass("hoverColor");
+        }
+    }
+    else {
+        if(!$(".num3").hasClass("activeNum")) {
+            $(".num3, .name3").toggleClass("hoverColor");
+        }
+    }
+});
